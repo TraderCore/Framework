adbd06f381048994c4517bddedde851c4a4c71b4
Thomas B <32000806+TheGoatedDev@users.noreply.github.com>
2025-02-02 01:02:03 +0000

    chore: update Jest configuration to use .cjs extension
    - Renamed jest.js to jest.cjs in jest-config package
    - Updated package.json to reference jest.cjs
    - Modified Jest config imports in multiple project configs to use .cjs extension

--- core/jest.config.cjs
+++ core/jest.config.cjs
@@ -1,4 +1,4 @@
-const jestConfig = require('@tradercore/jest-config/jest.js');
+const jestConfig = require('@tradercore/jest-config/jest.cjs');
 
 /** @type {import('jest').Config} */
 const config = {
